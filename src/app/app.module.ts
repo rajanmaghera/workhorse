99f482dc458dcf3eca49df27f59c255913b23ac2
Rajan Maghera <rmaghera@ualberta.ca>
2021-01-16 15:31:57 -0700

    Added Nebular theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { ManagerComponent } from './manager/manager.component';
 import { TimetableComponent } from './timetable/timetable.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { NbIconModule } from '@nebular/theme';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,13 @@ import { TimetableComponent } from './timetable/timetable.component';
     TimetableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbIconModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
