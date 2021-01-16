417a679a81e8a39741b8a8e41e6a360f35952227
Rajan Maghera <rmaghera@ualberta.ca>
2021-01-16 13:22:16 -0700

    Generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ManagerComponent } from './manager/manager.component';
+import { TimetableComponent } from './timetable/timetable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    ManagerComponent,
+    TimetableComponent
   ],
   imports: [
     BrowserModule
