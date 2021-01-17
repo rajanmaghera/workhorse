b495174c9e2469cc09396185fc9e92816d6b4c9f
Rajan Maghera <rmaghera@ualberta.ca>
2021-01-17 06:05:39 -0700

    Filled all pages with templates and content

--- src/app/payments-page/payments-page.component.ts
+++ src/app/payments-page/payments-page.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentsPageComponent implements OnInit {
 
+
+  payments: {date: string, payment: string, hours: string}[] = [
+    {date: "Wednesday, January 20", payment: "$720.00", hours: "40 hours"},
+    {date: "Wednesday, January 6", payment: "$731.00", hours: "40 hours"},
+    {date: "Friday, December 23", payment: "$718.00", hours: "40 hours"},
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/shifts-page/shifts-page.component.ts
+++ src/app/shifts-page/shifts-page.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShiftsPageComponent implements OnInit {
 
+  shifts: {date: string, time: string, length: string}[] = [
+    {date: "Wednesday, January 20", time: "11:00 AM", length: "4 hours"},
+    {date: "Thursday, January 21", time: "8:00 AM", length: "8 hours"},
+    {date: "Friday, January 22", time: "2:00 PM", length: "4 hours"},
+  ]
+
+
   constructor() { }
 
   ngOnInit(): void {
